8ac8058a4c2a61db4376e4c5eb6e0c0948b3766c
charleseMuchmore <87589934+charleseMuchmore@users.noreply.github.com>
2022-06-01 19:42:18 -0700

    screen setup

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+from turtle import Turtle, Screen
+
+# screen setup
+screen = Screen()
+screen.tracer(n=0, delay=0)
+screen.bgcolor("black")
+screen_setup_turtle = Turtle()
+screen_setup_turtle.color("white")
+screen_setup_turtle.penup()
+screen_setup_turtle.goto(0, 400)
+screen_setup_turtle.setheading(270)
+screen_setup_turtle.pensize(5)
+for x in range(27):
+    screen_setup_turtle.pendown()
+    screen_setup_turtle.forward(10)
+    screen_setup_turtle.penup()
+    screen_setup_turtle.forward(20)
+
+screen.exitonclick()
