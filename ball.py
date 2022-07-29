9699c7a64217b561fb3e735ee6b43d2aebeb9eb6
charleseMuchmore <87589934+charleseMuchmore@users.noreply.github.com>
2022-07-29 14:03:19 -0700

    Vscode branch (#2)
    * deleted unused files
    
    * created ball, it can move, still working on movement algorythm
    
    * improved and finished ball bounce algorythm
    
    * ball paddle collision detection created
    
    * tweaked the method used to reset the ball
    
    * added scoreboard and points tracking
    
    * ball speed increases with each paddle collision

---
+++ ball.py
@@ -0,0 +1,36 @@
+from re import U
+from turtle import Turtle
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.xmover = +10
+        self.ymover = +10
+        self.speed = 0.04
+
+    def move(self):
+        new_x = self.xcor() + self.xmover
+        new_y = self.ycor() + self.ymover
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.ymover *= -1
+        
+    def bounce_x(self):
+        self.xmover *= -1
+
+    def reset(self):
+        self.hideturtle()
+        self.goto(0,0)
+        self.bounce_x()
+        self.showturtle()
+
+    def speed_reset(self):
+        self.speed = 0.04 
+
+    def speed(self):
+        return(self.speed)
--- main_file.py
+++ main_file.py
@@ -1,6 +1,9 @@
 from turtle import Screen
 from paddle import Paddle
 from screen_setup import ScreenSetup
+from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -10,9 +13,13 @@ screen.title("Pong")
 screen.tracer(0)
 screen_setup_turtle = ScreenSetup()
 screen_setup_turtle.setup()
+screen_setup_turtle.draw_border()
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
 
 screen.listen()
 screen.onkey(r_paddle.down, "Down")
@@ -21,9 +28,31 @@ screen.onkey(l_paddle.down, "s")
 screen.onkey(l_paddle.up, "w")
 screen.tracer(True)
 
+left_player_score = 0
+right_player_score = 0
 game_is_on = True
+ball_speed = ball.speed
+print(type(ball_speed))
 # game loop
 while game_is_on:
+    time.sleep(ball_speed)
     screen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        ball_speed /= 2
+
+    if ball.xcor() > 380:
+        scoreboard.l_point()
+        ball.reset()
+        ball.speed_reset()
+    if ball.xcor() < -380:
+        scoreboard.r_point()
+        right_player_score += 1
+        ball.reset()
+        ball.speed_reset()
+
 
 screen.exitonclick()
